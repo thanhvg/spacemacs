cb24cdd6c4f99cd45a30cf24e18553ec8d3a959f
Thanh Vuong <GiaThanh.Vuong@Verdazo.com>
2020-02-25 17:34:07 -0700

    [js] new layer js to replace both javascript and react layers

--- layers/+lang/typescript/layers.el
+++ layers/+lang/typescript/layers.el
@@ -21,7 +21,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-(configuration-layer/declare-layer-dependencies '(node javascript prettier))
+(configuration-layer/declare-layer-dependencies '(node prettier))
 
 (when (boundp 'typescript-backend)
   (pcase typescript-backend
