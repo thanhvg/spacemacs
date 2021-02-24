7f264a3cb551c06e3217cc486716e99547d6ddde
Thanh Vuong <thanhvg@gmail.com>
2021-02-24 14:29:45 -0700

    [core] new function spacemacs/set-root-leader-keys-for-mode
    [core] This function allows to bind a mode command behind `dotspacemacs-leader-key' and
    `dotspacemacs-emacs-leader-key' when that mode is activate.
    
    [org] apply this function to org narrow commands
    
    motivation:
    
    Currently `spacemacs/set-leader-keys-for-major-mode` and
    `spacemacs/set-leader-keys-for-minor-mode` only let us bind command under `spc
    m` and `,` That works fine in general. But not all commands should go there. A
    mode that deals with compilation or comments when active should have commands
    bound behind `spc c` not `spc m`. Another example is org-mode narrow commands
    should go to prefix `spc n` not a long and tedious `spc m s b n`.

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -392,23 +392,28 @@ Will work on both org-mode and any mode that accepts plain html."
       "xu" (spacemacs|org-emphasize spacemacs/org-underline ?_)
       "xv" (spacemacs|org-emphasize spacemacs/org-verbatim ?=))
 
-    ;; Add global evil-leader mappings. Used to access org-agenda
-    ;; functionalities – and a few others commands – from any other mode.
-    (spacemacs/declare-prefix
-      "ao"  "org"
-      "aof" "feeds"
-      "aoC" (org-clocks-prefix))
-    ;; org-agenda
-    (when (configuration-layer/layer-used-p 'ivy)
-      (spacemacs/set-leader-keys "ao/" 'org-occur-in-agenda-files))
-    (spacemacs/set-leader-keys
-      "ao#" 'org-agenda-list-stuck-projects
-      "aoa" 'org-agenda-list
-      "aoo" 'org-agenda
-      "aoc" 'org-capture
-      "aoe" 'org-store-agenda-views
-      "aofi" 'org-feed-goto-inbox
-      "aofu" 'org-feed-update-all
+      (spacemacs/set-root-leader-keys-for-mode 'org-mode nil
+        "nb" 'org-narrow-to-block
+        "ns" 'org-narrow-to-subtree
+        "ne" 'org-narrow-to-element)
+
+      ;; Add global evil-leader mappings. Used to access org-agenda
+      ;; functionalities – and a few others commands – from any other mode.
+      (spacemacs/declare-prefix
+        "ao"  "org"
+        "aof" "feeds"
+        "aoC" (org-clocks-prefix))
+      ;; org-agenda
+      (when (configuration-layer/layer-used-p 'ivy)
+        (spacemacs/set-leader-keys "ao/" 'org-occur-in-agenda-files))
+      (spacemacs/set-leader-keys
+        "ao#" 'org-agenda-list-stuck-projects
+        "aoa" 'org-agenda-list
+        "aoo" 'org-agenda
+        "aoc" 'org-capture
+        "aoe" 'org-store-agenda-views
+        "aofi" 'org-feed-goto-inbox
+        "aofu" 'org-feed-update-all
 
       ;; Clock
       ;; These keybindings should match those under the "mC" prefix (above)
