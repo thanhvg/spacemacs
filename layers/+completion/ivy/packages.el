f01542c0807af6521637213fb7f3a55a68a5f4cd
Thanh Vuong <thanhvg@gmail.com>
2021-05-06 15:58:52 -0600

    [ivy] add evil-collection

--- layers/+completion/ivy/packages.el
+++ layers/+completion/ivy/packages.el
@@ -29,6 +29,7 @@
         counsel
         (counsel-projectile :requires projectile)
         evil
+        evil-collection
         flx
         helm-make
         imenu
@@ -207,6 +208,9 @@
 (defun ivy/post-init-evil ()
   (spacemacs/set-leader-keys
     "re" 'spacemacs/ivy-evil-registers))
+
+(defun ivy/pre-init-evil-collection ()
+  (add-to-list 'spacemacs-evil-collection-allowed-list 'ivy))
 
 (defun ivy/init-flx ()
   (use-package flx))
