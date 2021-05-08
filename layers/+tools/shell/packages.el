4e3cfa23f0edb75339bd3d9bf3af862cbeb81f47
Thanh Vuong <thanhvg@gmail.com>
2021-05-08 16:28:06 -0600

    [shell] vterm as repl

--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -348,7 +348,12 @@
     (progn
       (make-shell-pop-command "vterm" vterm)
       (spacemacs/set-leader-keys "atsv" 'spacemacs/shell-pop-vterm)
-      (spacemacs/register-repl 'vterm 'vterm))
+      (spacemacs/register-repl 'vterm 'vterm)
+      (spacemacs/set-leader-keys "psf" #'spacemacs/vterm-repl-send-function)
+      (spacemacs/set-leader-keys "psl" #'spacemacs/vterm-repl-send-line)
+      (spacemacs/set-leader-keys "pss" #'spacemacs/vterm-repl-send-dwim)
+      (spacemacs/set-leader-keys "psr" #'spacemacs/vterm-repl-send-region)
+      (spacemacs/set-leader-keys "psg" #'spacemacs/vterm-repl-send-buffer))
     :config
     (progn
       (setq vterm-shell shell-default-term-shell)
