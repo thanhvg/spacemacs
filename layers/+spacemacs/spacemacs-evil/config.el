146cb63fbe8e396ff6034a542e4469a0b3448f93
Thanh Vuong <thanhvg@gmail.com>
2021-11-01 01:03:10 -0600

    [evil] enable xref for evil-collection

--- layers/+spacemacs/spacemacs-evil/config.el
+++ layers/+spacemacs/spacemacs-evil/config.el
@@ -31,6 +31,5 @@
 (defvar evil-lisp-safe-structural-editing-modes '()
   "A list of major mode symbols where safe structural editing is supported.")
 
-(defvar spacemacs-evil-collection-allowed-list
-  '(dired ediff eww info quickrun replace simple bookmark)
+(defvar spacemacs-evil-collection-allowed-list '(dired ediff eww info quickrun replace simple bookmark xref)
   "List of modes Spacemacs will allow to be evilified by ‘evil-collection-init’.")
