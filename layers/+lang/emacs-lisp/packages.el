29b0073ba3e18595e7128801e04c19b1570215c6
Thanh Vuong <thanhvg@gmail.com>
2021-12-20 21:24:00 -0700

    [elisp] add elip-def
    this package provide highly acurate jump to the definition, it even work with
    local variable

--- layers/+lang/emacs-lisp/packages.el
+++ layers/+lang/emacs-lisp/packages.el
@@ -158,7 +158,7 @@
 
 (defun emacs-lisp/init-elisp-def ()
   (use-package elisp-def
-    :defer t))
+    :defer (spacemacs/defer)))
 
 (defun emacs-lisp/init-elisp-slime-nav ()
   ;; Elisp go-to-definition with M-. and back again with M-,
