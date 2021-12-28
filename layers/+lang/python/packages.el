fdeff2e94ab999c65d7e5c5edb7f68ee32025c19
Thanh Vuong <thanhvg@gmail.com>
2021-12-28 07:54:59 -0700

    [python] remove importmagic

--- layers/+lang/python/packages.el
+++ layers/+lang/python/packages.el
@@ -36,7 +36,6 @@
     helm-cscope
     helm-gtags
     (helm-pydoc :requires helm)
-    importmagic
     live-py-mode
     (nose :location (recipe :fetcher github :repo "syl20bnr/nose.el"))
     org
@@ -181,16 +180,6 @@
     :defer t
     :init
     (spacemacs/set-leader-keys-for-major-mode 'python-mode "hd" 'helm-pydoc)))
-
-(defun python/init-importmagic ()
-  (use-package importmagic
-    :defer t
-    :init
-    (progn
-      (add-hook 'python-mode-hook 'importmagic-mode)
-      (spacemacs|diminish importmagic-mode " ⓘ" " [i]")
-      (spacemacs/set-leader-keys-for-major-mode 'python-mode
-        "rf" 'importmagic-fix-symbol-at-point))))
 
 (defun python/init-live-py-mode ()
   (use-package live-py-mode
