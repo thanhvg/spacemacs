35f05bfa6db608dd4053864f568d63c760b49bad
Thanh Vuong <thanhvg@gmail.com>
2022-01-08 22:55:01 -0700

    [tree-sitter] add option to seletively disable tree sitter hl mode

--- layers/+tools/tree-sitter/config.el
+++ layers/+tools/tree-sitter/config.el
@@ -32,5 +32,6 @@
 (defvar tree-sitter-fold-indicators-enable t
   "If non nil, and `tree-sitter-fold-enable' is non nil, show fold indicators in fringe.")
 
-(defvar spacemacs-tree-sitter-hl-black-list nil
-  "List of major modes where `tree-sitter-hl-mode' is disabled.")
+(defvar spacemacs-tree-sitter-hl-black-list
+  '()
+  "List of major modes not allow tree sitter highlight mode.")
