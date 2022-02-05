4aafdca085ca72c3704891ca0b3d04885a34d2c6
Thanh <thanhvg@gmail.com>
2022-02-04 17:57:03 -0700

    [auto-completion] add company-posframe

--- layers/+completion/auto-completion/packages.el
+++ layers/+completion/auto-completion/packages.el
@@ -25,6 +25,7 @@
       '(
         auto-yasnippet
         company
+        (company-posframe :toggle auto-completion-use-company-posframe)
         (company-box :toggle auto-completion-use-company-box)
         (all-the-icons :toggle auto-completion-use-company-box)
         (company-quickhelp :toggle auto-completion-enable-help-tooltip)
@@ -216,6 +217,13 @@
         ('manual (define-key company-active-map
                    (kbd "M-h") #'company-box-doc-manually))
         ('t (setq company-box-doc-enable t))))))
+
+(defun auto-completion/init-company-posframe ()
+  (use-package company-posframe
+    :hook '(company-mode . company-posframe-mode)
+    :config
+    (progn
+      (spacemacs|hide-lighter company-posframe-mode))))
 
 (defun auto-completion/init-helm-c-yasnippet ()
   (use-package helm-c-yasnippet
