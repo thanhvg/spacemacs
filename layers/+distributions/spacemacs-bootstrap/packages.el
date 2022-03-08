33e4cd4bd2dc0c6e3623364241416c1e6855bb08
Thanh Vuong <thanhvg@gmail.com>
2022-03-07 20:03:45 -0700

    Revert "Open special-mode buffer in motion-state (not evilified)"
    This reverts commit b1fe17d370d5a3f3b7c8d69b51f7b12b77368418.
    
    not a good idea to put everyone on motion state

--- layers/+distributions/spacemacs-bootstrap/packages.el
+++ layers/+distributions/spacemacs-bootstrap/packages.el
@@ -293,11 +293,6 @@
     (list (point-min) (point-max)))
   (define-key evil-inner-text-objects-map "g" 'evil-inner-buffer)
 
-  ;; special-mode buffers are read-only and therefore should open in
-  ;; motion-state (evilified state is too aggressive, e.g. evil-local-set-key
-  ;; has no effect)
-  (add-to-list 'evil-motion-state-modes 'special-mode)
-
   ;; turn off evil in corelv buffers
   (add-to-list 'evil-buffer-regexps '("\\*LV\\*"))
 
@@ -592,7 +587,9 @@ Press \\[which-key-toggle-persistent] to hide."
 ;; pre packages
 
 (defun spacemacs-bootstrap/init-evil-evilified-state ()
-  (use-package evil-evilified-state)
+  (use-package evil-evilified-state
+    :config
+    (add-to-list 'evil-evilified-state-modes 'special-mode))
   (define-key evil-evilified-state-map (kbd dotspacemacs-leader-key)
     spacemacs-default-map))
 
