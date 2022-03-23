36854cc5838ee2e72754b8bf5ce49bc344dae59e
Thanh Vuong <thanhvg@gmail.com>
2022-03-22 23:44:48 -0600

    [all] defer loading packages

--- layers/+lang/clojure/packages.el
+++ layers/+lang/clojure/packages.el
@@ -621,6 +621,7 @@
 
 (defun clojure/init-flycheck-clj-kondo ()
   (use-package flycheck-clj-kondo
+    :defer t
     :if (configuration-layer/package-usedp 'flycheck)))
 
 (defun clojure/init-flycheck-joker ()
