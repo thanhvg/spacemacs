c9cc58ba48d1420fac1e92d9376439db83d1833c
Thanh Vuong <thanhvg@gmail.com>
2022-03-22 23:44:48 -0600

    [all] defer loading packages

--- layers/+lang/clojure/packages.el
+++ layers/+lang/clojure/packages.el
@@ -620,6 +620,7 @@
 
 (defun clojure/init-flycheck-clj-kondo ()
   (use-package flycheck-clj-kondo
+    :defer t
     :if (configuration-layer/package-usedp 'flycheck)))
 
 (defun clojure/init-flycheck-joker ()
--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -287,6 +287,7 @@
 
 (defun shell/init-xterm-color ()
   (use-package xterm-color
+    :defer t
     :init
     ;; Comint and Shell
     (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
