5d86499ce7da229a4ecf22bc0f5eef2ecb20a360
Thanh <thanhvg@gmail.com>
2022-04-10 16:04:07 -0600

    [shell] add back vterm-extra

--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -44,7 +44,11 @@
     vi-tilde-fringe
     window-purpose
     (multi-vterm :toggle (and module-file-suffix (not (spacemacs/system-is-mswindows))))
-    (vterm :toggle (and module-file-suffix (not (spacemacs/system-is-mswindows))))))
+    (vterm :toggle (and module-file-suffix (not (spacemacs/system-is-mswindows))))
+    (vterm-extra
+     :location (recipe
+                :fetcher github
+                :repo  "Sbozzolo/vterm-extra"))))
 
 
 (defun shell/init-comint ()
@@ -409,3 +413,10 @@
                                   (eshell-mode . terminal)
                                   (shell-mode . terminal)
                                   (term-mode . terminal)))))
+
+(defun shell/init-vterm-extra ()
+  (use-package vterm-extra
+    :after vterm
+    :bind (("C-c $" . vterm-extra-dispatcher)
+           :map vterm-mode-map
+           (("C-c C-e" . vterm-extra-edit-command-in-new-buffer)))))
