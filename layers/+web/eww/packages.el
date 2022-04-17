f20d53debc91463f28a2212619fef30cbbcf74f6
Thanh Vuong <thanhvg@gmail.com>
2022-04-17 15:00:39 -0600

    [eww] tweak

--- layers/+web/eww/packages.el
+++ layers/+web/eww/packages.el
@@ -43,6 +43,7 @@
     (spacemacs//eww-setup-transient-state)
     (spacemacs/declare-prefix "awe" "eww")
     (spacemacs/set-leader-keys "awee" 'eww)
+    (spacemacs/set-leader-keys "aweb" 'eww-list-bookmarks)
     (spacemacs/set-leader-keys "awew" 'eww-switch-to-buffer)
     :config
     (define-key eww-link-keymap "f" 'eww-follow-link)
