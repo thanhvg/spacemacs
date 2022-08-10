5c28902bb2286759b245f60d7785b192047c285e
Thanh Vuong <thanhvg@gmail.com>
2022-08-09 22:11:38 -0600

    [shell] add native-complete

--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -25,6 +25,7 @@
   '(
     (comint :location built-in)
     company
+    company-native-complete
     consult
     eat
     esh-help
@@ -59,7 +60,16 @@
   (with-eval-after-load 'centered-cursor-mode
     (add-hook 'comint-mode-hook 'spacemacs//inhibit-global-centered-cursor-mode)))
 
+(defun shell/init-company-native-complete()
+  (use-package company-native-complete
+    :defer t))
+
 (defun shell/pre-init-company ()
+  (spacemacs|add-company-backends :backends company-native-complete :modes shell-mode)
+  (spacemacs|use-package-add-hook company-native-complete
+    :post-init
+    (with-eval-after-load 'shell
+      (native-complete-setup-bash)))
   ;; support in eshell
   (spacemacs|use-package-add-hook eshell
     :post-init
