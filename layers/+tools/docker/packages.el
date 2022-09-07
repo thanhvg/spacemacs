43290f48565a7e234ccf892c6ba918ac40db6515
Thanh <thanhvg@gmail.com>
2022-09-06 20:43:37 -0600

    Revert "Docker: Followup for #15414: fix bindings not loading, add docs (#15707)"
    This reverts commit 99eb20e13c3b698bb8ba95ed278fb79861625f5b.

--- layers/+tools/docker/packages.el
+++ layers/+tools/docker/packages.el
@@ -50,15 +50,14 @@
     :defer t
     :init (add-hook 'dockerfile-mode-local-vars-hook #'spacemacs//docker-dockerfile-setup-backend)
     :config
-    (progn
+    (spacemacs/set-leader-keys-for-major-mode 'dockerfile-mode
+      (if (null docker-dockerfile-backend) "b" "cb") 'dockerfile-build-buffer
+      (if (null docker-dockerfile-backend) "B" "cB") 'dockerfile-build-buffer-no-cache-buffer)
+    (with-eval-after-load 'docker
       (spacemacs/set-leader-keys-for-major-mode 'dockerfile-mode
-        (if (null docker-dockerfile-backend) "b" "cb") 'dockerfile-build-buffer
-        (if (null docker-dockerfile-backend) "B" "cB") 'dockerfile-build-buffer-no-cache-buffer)
-      (if (package-installed-p 'docker)
-        (spacemacs/set-leader-keys-for-major-mode 'dockerfile-mode
-          "d" 'docker
-          "i" 'docker-images
-          "p" 'docker-containers)))))
+        "d" 'docker
+        "i" 'docker-images
+        "p" 'docker-containers))))
 
 (defun docker/post-init-flycheck ()
   (spacemacs/enable-flycheck 'dockerfile-mode))
