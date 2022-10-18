75eddd5b9096ef01761b819cfd96f0b72d7554cb
Thanh Vuong <thanhvg@gmail.com>
2022-10-18 00:07:54 -0600

    [compleseus] tweak imenu

--- layers/+completion/compleseus/packages.el
+++ layers/+completion/compleseus/packages.el
@@ -310,9 +310,7 @@
       (evil-add-command-properties command :jump t)))
 
   ;; Configure consult-imenu for java-mode.
-  (use-package consult-imenu
-    :after consult
-    :config
+  (with-eval-after-load 'consult-imenu
     (add-to-list 'consult-imenu-config '(java-mode :toplevel "Classes" :types
                                                    ((?m "Methods" font-lock-function-name-face)
                                                     (?f "Fields" font-lock-variable-name-face)
