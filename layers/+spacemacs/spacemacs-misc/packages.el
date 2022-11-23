0c96d3931eac3c99854010600d08abb4c9724ec1
Thanh Vuong <thanhvg@gmail.com>
2022-11-22 23:29:24 -0700

    [misc] improve stock devdocs

--- layers/+spacemacs/spacemacs-misc/packages.el
+++ layers/+spacemacs/spacemacs-misc/packages.el
@@ -52,5 +52,6 @@
     :defer t
     :init
     (progn
+      (spacemacs/set-leader-keys "hbb" #'devdocs-lookup ) 
       (defalias 'spacemacs/browse-docs-online-at-point 'devdocs-search)
       (spacemacs/set-leader-keys "hbd" #'spacemacs/browse-docs-online-at-point))))
