2601b0cb2b17dd8186036cfed05a1d5efdfbf5cf
Thanh <thanhvg@gmail.com>
2022-12-16 15:22:54 -0700

    [completion] embark is owned in base

--- layers/+spacemacs/spacemacs-completion/funcs.el
+++ layers/+spacemacs/spacemacs-completion/funcs.el
@@ -324,3 +324,35 @@ See https://github.com/syl20bnr/spacemacs/issues/3700"
  [t]          open in other frame
  [v]          open in a new vertical split
  [q]          quit")
+
+;; which-key integration functions for embark
+;; https://github.com/oantolin/embark/wiki/Additional-Configuration#use-which-key-like-a-key-menu-prompt
+(defun spacemacs/embark-which-key-indicator ()
+  "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+  (lambda (&optional keymap targets prefix)
+    (if (null keymap)
+        (which-key--hide-popup-ignore-command)
+      (which-key--show-keymap
+       (if (eq (plist-get (car targets) :type) 'embark-become)
+           "Become"
+         (format "Act on %s '%s'%s"
+                 (plist-get (car targets) :type)
+                 (embark--truncate-target (plist-get (car targets) :target))
+                 (if (cdr targets) "…" "")))
+       (if prefix
+           (pcase (lookup-key keymap prefix 'accept-default)
+             ((and (pred keymapp) km) km)
+             (_ (key-binding prefix 'accept-default)))
+         keymap)
+       nil nil t (lambda (binding)
+                   (not (string-suffix-p "-argument" (cdr binding))))))))
+
+(defun spacemacs/embark-hide-which-key-indicator (fn &rest args)
+  "Hide the which-key indicator immediately when using the completing-read prompter."
+  (which-key--hide-popup-ignore-command)
+  (let ((embark-indicators
+         (remq #'spacemacs/embark-which-key-indicator embark-indicators)))
+    (apply fn args)))
