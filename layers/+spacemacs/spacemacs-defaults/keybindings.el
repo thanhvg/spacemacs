bfc98b564e0242a7ab5ebb2f986acc68808532cc
Thanh Vuong <thanhvg@gmail.com>
2022-12-17 12:01:07 -0700

    [editing] avy go to symbol at point
    bind to "jo"
    move open line to "io"

--- layers/+spacemacs/spacemacs-defaults/keybindings.el
+++ layers/+spacemacs/spacemacs-defaults/keybindings.el
@@ -435,6 +435,7 @@
   "hTe" 'emacs-tutorial)
 ;; insert stuff ---------------------------------------------------------------
 (spacemacs/set-leader-keys
+  "io" 'open-line
   "iJ" 'spacemacs/insert-line-below-no-indent
   "iK" 'spacemacs/insert-line-above-no-indent
   "ik" 'spacemacs/evil-insert-line-above
@@ -445,7 +446,6 @@
   "j(" 'check-parens
   "j=" 'spacemacs/indent-region-or-buffer
   "j+" 'spacemacs/iwb-region-or-buffer
-  "jo" 'open-line
   "jS" 'spacemacs/split-and-new-line
   "jk" 'spacemacs/evil-goto-next-line-and-indent)
 
