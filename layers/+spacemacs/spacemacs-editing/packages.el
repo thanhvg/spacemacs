3b76518eed67524967410f8846e37a716a8c604a
Thanh Vuong <thanhvg@gmail.com>
2022-12-17 12:01:07 -0700

    [editing] avy go to symbol at point
    bind to "jo"
    move open line to "io"

--- layers/+spacemacs/spacemacs-editing/packages.el
+++ layers/+spacemacs/spacemacs-editing/packages.el
@@ -87,10 +87,10 @@
         "jb" 'avy-pop-mark
         "jj" 'evil-avy-goto-char-timer
         "jl" 'evil-avy-goto-line
+        "jo" 'spacemacs/avy-goto-symbol-at-point
         "ju" 'spacemacs/avy-goto-url
         "jU" 'spacemacs/avy-open-url
-        "jw" 'evil-avy-goto-word-or-subword-1
-        "xo" 'spacemacs/avy-open-url))
+        "jw" 'evil-avy-goto-word-or-subword-1))
     :config
     (progn
       (defun spacemacs/avy-goto-url ()
