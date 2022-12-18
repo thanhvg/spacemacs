d976d5848fde44f2e3b055234342241c2eab7b98
Thanh Vuong <thanhvg@gmail.com>
2022-12-17 17:00:23 -0700

    [default] new occur symbol at point bound to spc sn

--- layers/+spacemacs/spacemacs-defaults/funcs.el
+++ layers/+spacemacs/spacemacs-defaults/funcs.el
@@ -1985,3 +1985,17 @@ Show current active buffer in another buffer."
   (interactive)
   (with-selected-window (or (minibuffer-selected-window) (selected-window))
     (display-buffer (current-buffer) '(nil (inhibit-same-window . t)))))
+
+
+(defun spacemacs/occur-symbol-at-point (symbol &optional nlines region)
+  "Same as `occur' but on symbol at point."
+  (interactive
+   (nconc (list
+           (thing-at-point 'symbol t)
+           (when current-prefix-arg
+             (prefix-numeric-value current-prefix-arg))
+           (and (use-region-p) (list (region-bounds))))))
+  (funcall 'occur
+           (concat "\\_<" (regexp-quote symbol) "\\_>")
+           nlines
+           region))
