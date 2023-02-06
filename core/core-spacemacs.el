89b83030e34c8ab8aab66e134d3d7ed63ec1d102
Thanh <thanhvg@gmail.com>
2023-02-06 16:42:30 -0700

    [org] revert org 9.6

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -65,7 +65,6 @@
 
 (defvar spacemacs--default-mode-line mode-line-format
   "Backup of default mode line format.")
-
 (defvar spacemacs-initialized nil
   "Whether or not spacemacs has finished initializing by completing
 the final step of executing code in `emacs-startup-hook'.")
@@ -158,14 +157,17 @@ the final step of executing code in `emacs-startup-hook'.")
         (car dotspacemacs-default-font)))))
   ;; spacemacs init
   (setq inhibit-startup-screen t)
-
-  ;; Draw the spacemacs buffer without lists and scalling to avoid having
-  ;; to load build-in org which will conflict with elpa org
-  (spacemacs-buffer/goto-buffer t)
-
+  (spacemacs-buffer/goto-buffer)
+  (unless (display-graphic-p)
+    ;; explicitly recreate the home buffer for the first GUI client
+    ;; in order to correctly display the logo
+    (spacemacs|do-after-display-system-init
+     (kill-buffer (get-buffer spacemacs-buffer-name))
+     (spacemacs-buffer/goto-buffer)))
   ;; This is set to nil during startup to allow Spacemacs to show buffers opened
   ;; as command line arguments.
   (setq initial-buffer-choice nil)
+  (setq inhibit-startup-screen t)
   (require 'core-keybindings)
   ;; for convenience and user support
   (unless (fboundp 'tool-bar-mode)
@@ -259,18 +261,13 @@ Note: the hooked function is not executed when in dumped mode."
      (setq spacemacs-initialized t)
      (setq gc-cons-threshold (car dotspacemacs-gc-cons)
            gc-cons-percentage (cadr dotspacemacs-gc-cons))
-     (setq read-process-output-max dotspacemacs-read-process-output-max)
-     ;; Redraw the spacemacs buffer with full org support
-     ;; Before it must be drawn without org related features to
-     ;; avoid loading build in org in emacs >= 29
-     (spacemacs-buffer/goto-buffer t t)))
+     (setq read-process-output-max dotspacemacs-read-process-output-max)))
 
   (if dotspacemacs-byte-compile
       (when (> 1 (spacemacs//dir-byte-compile-state
                   (concat spacemacs-core-directory "libs/")))
         (byte-recompile-directory (concat spacemacs-core-directory "libs/") 0))
     (spacemacs//remove-byte-compiled-files-in-dir spacemacs-core-directory))
-
   ;; Check if revision has changed.
   (spacemacs//revision-check))
 
