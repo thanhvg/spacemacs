fe6321918a8404a2c6a424aa0c07a9987651cc05
Thanh <thanhvg@gmail.com>
2023-02-06 16:44:10 -0700

    [org] use built-in

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -35,7 +35,7 @@
     htmlize
     ;; ob, org, org-agenda and org-contacts are installed by `org-contrib'
     (ob :location built-in)
-    (org :location elpa :min-version "9.5")
+    (org :location built-in)
     (org-agenda :location built-in)
     (org-wild-notifier
                 :toggle org-enable-notifications)
