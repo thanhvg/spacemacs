f1f2992859bc045c1bfed52f911a93a4885959c1
Thanh Vuong <thanhvg@gmail.com>
2023-02-27 09:00:44 -0700

    [lspce] new layer lspce

---
+++ layers/+tools/lspce/packages.el
@@ -0,0 +1,56 @@
+;;; packages.el --- lspce layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2022 Sylvain Benner & Contributors
+;;
+;; Author: Thanh Vuong <thanh@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `lspce-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `lspce/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `lspce/pre-init-PACKAGE' and/or
+;;   `lspce/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst lspce-packages
+  '((lspce :location built-in)))
+
+(defun lspce/init-lspce ()
+  (use-package lspce
+    :load-path "~/git/lspce"
+    :config
+    (add-hook 'lspce-mode-hook
+              (lambda ()
+                (setq-local company-backends
+                            (cl-adjoin
+                             'company-capf company-backends
+                             :test
+                             #'equal))))))
