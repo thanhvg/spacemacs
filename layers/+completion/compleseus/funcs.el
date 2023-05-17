4bfa6d648943043bf52f4d9f6ad4dcf3cabb76e6
Thanh Vuong <thanhvg@gmail.com>
2023-05-17 13:11:43 -0600

    [compleseus] `C-/` spacemacs/consult-company for company

--- layers/+completion/compleseus/funcs.el
+++ layers/+completion/compleseus/funcs.el
@@ -236,6 +236,22 @@ to act on with `embark-act-all', and move to the next candidate."
              (car (nth (1+ idx) narrow-keys))))
        (caar narrow-keys)))))
 
+(defun spacemacs/consult-company ()
+  "Complete using `company-candidates'."
+  (interactive)
+  (company-mode 1)
+  (unless company-candidates
+    (company-complete))
+  (when company-candidates
+    (company--continue)
+    (company-finish (completing-read "Candidate: " company-candidates))))
+
+(defun spacemacs/compleseus-grep-change-to-wgrep-mode ()
+  (interactive)
+  (require 'wgrep)
+  (wgrep-change-to-wgrep-mode)
+  (evil-normal-state))
+
 (defun spacemacs/consult-edit ()
   "Export the consult buffer and make the buffer editable righ away."
   (interactive)
