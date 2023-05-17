4bc11468abc95c832bc19666ab50b7c09c60b1e5
Thanh Vuong <thanhvg@gmail.com>
2023-05-17 13:11:43 -0600

    [compleseus] `C-/` spacemacs/consult-company for company

--- layers/+completion/compleseus/funcs.el
+++ layers/+completion/compleseus/funcs.el
@@ -68,7 +68,7 @@ non-nil."
                                (or (thing-at-point 'symbol t) "")))
                           ""))
          (default-directory
-           (or initial-directory (read-directory-name "Start from directory: "))))
+          (or initial-directory (read-directory-name "Start from directory: "))))
     (consult-ripgrep default-directory initial-input)))
 
 (defun spacemacs/consult-line ()
@@ -185,6 +185,16 @@ This solves the problem: Binding a key to: `find-file' calls: `ido-find-file'"
    (cond
     ((eq major-mode 'org-mode) 'consult-org-heading)
     (t 'consult-imenu))))
+
+(defun spacemacs/consult-company ()
+  "Complete using `company-candidates'."
+  (interactive)
+  (company-mode 1)
+  (unless company-candidates
+    (company-complete))
+  (when company-candidates
+    (company--continue)
+    (company-finish (completing-read "Candidate: " company-candidates))))
 
 (defun spacemacs/compleseus-grep-change-to-wgrep-mode ()
   (interactive)
