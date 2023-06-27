779bd7ac8a49f0859217881001f727619319190b
Thanh Vuong <thanhvg@gmail.com>
2023-06-27 17:01:26 -0600

    [compleseus] tweak `spc bb`

--- layers/+completion/compleseus/funcs.el
+++ layers/+completion/compleseus/funcs.el
@@ -43,9 +43,9 @@
    '(consult--source-hidden-buffer
      consult--source-persp-buffers
      consult--source-modified-buffers
-     consult--source-recent-file
-     consult--source-bookmark
-     consult--source-project-buffer
+     ;; consult--source-recent-file
+     ;; consult--source-bookmark
+     ;; consult--source-project-buffer
      consult--source-project-recent-file)))
 
 
