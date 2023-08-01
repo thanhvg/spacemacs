fa26df25fab11fe7b9c7f98da158e99597efa838
Thanh Vuong <thanhvg@gmail.com>
2023-07-31 23:15:40 -0600

    [mermaid] new layer

---
+++ layers/+lang/mermaid/packages.el
@@ -0,0 +1,60 @@
+;;; packages.el --- mermaid layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2022 Sylvain Benner & Contributors
+;;
+;; Author: Thanh Vuong <thanh@x230>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `mermaid-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `mermaid/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `mermaid/pre-init-PACKAGE' and/or
+;;   `mermaid/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst mermaid-packages
+  '(org
+    mermaid-mode
+    ;; ob-mermaid
+    (ob-mermaid :location local))
+  "The list of Lisp packages required by the mermaid layer.")
+
+(defun mermaid/init-mermaid-mode ()
+  (use-package mermaid-mode
+    :defer t))
+
+(defun mermaid/post-init-org ()
+  (spacemacs|use-package-add-hook org
+    :post-config (add-to-list 'org-babel-load-languages '(mermaid . t))))
+
+(defun mermaid/init-ob-mermaid ()
+  (use-package ob-mermaid
+    :defer t))
