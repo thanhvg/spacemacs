8aaec846a64706887565e1b936d4458eca131649
Thanh Vuong <thanhvg@gmail.com>
2023-08-29 22:37:41 -0600

    [java] add bindings to java-mode to java-ts-mode

--- layers/+lang/java/funcs.el
+++ layers/+lang/java/funcs.el
@@ -20,6 +20,11 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+(defun spacemacs//java-mode ()
+  "Return desired java mode."
+  (if java-use-ts-mode
+      'java-ts-mode
+    'java-mode))
 
 (defun spacemacs//java-setup-backend ()
   "Conditionally setup java backend."
