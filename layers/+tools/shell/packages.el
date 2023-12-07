02fd59d5676603a3dbb910e9fdd003a648a5ce00
Thanh <thanhvg@gmail.com>
2023-12-06 22:24:57 -0700

    [shell] fix vterm evil insert state cursor position

--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -410,6 +410,7 @@
       (kbd "C-k") 'vterm-send-up)
 
     (add-hook 'vterm-mode-hook 'spacemacs/disable-hl-line-mode)
+    (add-hook 'vterm-mode-hook #'spacemacs//evil-vterm-compat)
     (with-eval-after-load 'centered-cursor-mode
       (add-hook 'vterm-mode-hook 'spacemacs//inhibit-global-centered-cursor-mode))))
 
