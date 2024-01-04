bb4d63c65b20700a5f0e08a478ed27804e4059fd
Thanh <thanhvg@gmail.com>
2024-01-04 08:47:51 -0700

    [evil] enable evil-collection for info

--- layers/+spacemacs/spacemacs-evil/config.el
+++ layers/+spacemacs/spacemacs-evil/config.el
@@ -31,5 +31,5 @@
 (defvar evil-lisp-safe-structural-editing-modes '()
   "A list of major mode symbols where safe structural editing is supported.")
 
-(defvar spacemacs-evil-collection-allowed-list '(dired ediff eww quickrun replace simple xref devdocs)
+(defvar spacemacs-evil-collection-allowed-list '(dired ediff eww quickrun replace simple xref devdocs info)
   "List of modes Spacemacs will allow to be evilified by ‘evil-collection-init’.")
