7979bfa1c06a819f789c4c7dc4164c8c76fab7b6
Thanh Vuong <thanhvg@gmail.com>
2024-04-24 05:32:45 -0600

    [lsp-bridge] wip

--- layers/+tools/lsp-bridge/funcs.el
+++ layers/+tools/lsp-bridge/funcs.el
@@ -1,2 +1,6 @@
 (defun spacemacs//lsp-bridge-turn-off-incompatible-mode ()
   (company-mode -1))
+
+(defun spacemacs//lsp-bridge-setup ()
+  (company-mode -1)
+  (add-to-list 'spacemacs-jump-handlers '(lsp-bridge-find-def :async t)))
