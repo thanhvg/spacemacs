6104ba4b6643350969310ed52d6379b00f663301
Thanh Vuong <thanhvg@gmail.com>
2024-06-01 19:59:05 -0600

    [evil] Revert "temporarily pinned evil because it breaks evil-mc"
    This reverts commit 488fec9aeb4bec32babe1b479bbbd7093ecd73aa.

--- layers/+distributions/spacemacs-bootstrap/packages.el
+++ layers/+distributions/spacemacs-bootstrap/packages.el
@@ -29,13 +29,7 @@
     (bind-map :step bootstrap)
     (bind-key :step bootstrap)
     (diminish :step bootstrap)
-    (evil :step bootstrap
-          :location (recipe
-                     :fetcher github
-                     :repo  "emacs-evil/evil"
-                     :commit "e7f170fb1f08662b5a4165adc23d84858129e3ef"))
-                     ;; break evil-mc
-                     ;; :commit "2b2ba3cbeabe1f239b6b0ebdaddcb68dd158bd1f"))
+    (evil :step bootstrap)
     (hydra :step bootstrap)
     (use-package :step bootstrap)
     (which-key :step bootstrap)
