63179181a1067b26e504f898827c4af4ac660e56
Thanh Vuong <thanhvg@gmail.com>
2024-06-20 10:53:30 -0600

    [java] command to copy full method name

--- layers/+lang/java/packages.el
+++ layers/+lang/java/packages.el
@@ -136,6 +136,8 @@
       "gku" 'spacemacs/lsp-java-super-type
       "gks" 'spacemacs/lsp-java-sub-type
 
+      "yn" 'spacemacs/copy-full-qualified-name
+
       ;; test
       "tb" 'lsp-jt-browser)))
 
