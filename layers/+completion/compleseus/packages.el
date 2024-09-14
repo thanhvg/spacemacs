9beb88b4881840b8e46887e0659a1ee53948c4dd
Thanh <thanhvg@gmail.com>
2024-09-14 16:21:55 -0600

    [git][compleseus] don't hide magit buffers
    - don't set magit buffers as spacemacs-useless-buffers-regexp, SPC tab will go
    back to magit buffers
    
    - don't add magit buffers to consult-buffer-filter, consult buffer will magit
    buffers, won't treat them as hidden buffers

--- layers/+completion/compleseus/packages.el
+++ layers/+completion/compleseus/packages.el
@@ -286,7 +286,7 @@
      :preview-key (list "C-SPC" "C-M-j" "C-M-k"))
 
     ;; hide magit buffer
-    (add-to-list 'consult-buffer-filter "magit.*:.*")
+    ;; (add-to-list 'consult-buffer-filter "magit.*:.*")
 
     ;; Optionally configure the narrowing key.
     ;; Both < and C-+ work reasonably well.
