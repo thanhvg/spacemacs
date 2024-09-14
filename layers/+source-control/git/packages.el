73b488f5f552d0fd2177f83f493c682473fe8aab
Thanh <thanhvg@gmail.com>
2024-09-14 16:21:55 -0600

    [git][compleseus] don't hide magit buffers
    - don't set magit buffers as spacemacs-useless-buffers-regexp, SPC tab will go
    back to magit buffers
    
    - don't add magit buffers to consult-buffer-filter, consult buffer will magit
    buffers, won't treat them as hidden buffers

--- layers/+source-control/git/packages.el
+++ layers/+source-control/git/packages.el
@@ -149,8 +149,8 @@
     :defer (spacemacs/defer)
     :custom (magit-bury-buffer-function #'magit-restore-window-configuration)
     :init
-    (push "magit: .*" spacemacs-useless-buffers-regexp)
-    (push "magit-.*: .*"  spacemacs-useless-buffers-regexp)
+    ;; (push "magit: .*" spacemacs-useless-buffers-regexp)
+    ;; (push "magit-.*: .*"  spacemacs-useless-buffers-regexp)
     (spacemacs|require-when-dumping 'magit)
     (setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     "))
     ;; On Windows, we must use Git GUI to enter username and password
