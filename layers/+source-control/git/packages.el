49763efb6e60707ad9a01380d7d0d2aa26061c45
Thanh <thanhvg@gmail.com>
2024-09-14 16:34:34 -0600

    [git] refactor how to set persp-filter-save-buffers-functions

--- layers/+source-control/git/packages.el
+++ layers/+source-control/git/packages.el
@@ -46,6 +46,7 @@
     org
     (orgit :requires org)
     (orgit-forge :requires (org forge))
+    persp-mode
     smeargle
     transient))
 
@@ -201,9 +202,7 @@
       ("Y" magit-blame-copy-hash)
       ("B" magit-blame :exit t)
       ("Q" nil :exit t))
-    (with-eval-after-load 'persp-mode
-      (add-hook 'persp-filter-save-buffers-functions
-                'spacemacs//magit-buffer-p))
+
     :config
     ;; seems to be necessary at the time of release
     (require 'git-rebase)
@@ -370,3 +369,9 @@
       "c" 'forge-post-submit
       "k" 'forge-post-cancel
       "a" 'forge-post-cancel)))
+
+(defun git/pre-init-persp-mode ()
+  (spacemacs|use-package-add-hook persp-mode
+    :post-config
+    (add-to-list 'persp-filter-save-buffers-functions
+                 'spacemacs//magit-buffer-p)))
