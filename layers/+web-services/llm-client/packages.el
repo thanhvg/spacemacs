c480e1393539b1c1a5fa3384396d956d7c962df1
Thanh <thanhvg@gmail.com>
2025-06-02 23:04:28 -0600

    [llm-client] add aider and minuet

--- layers/+web-services/llm-client/packages.el
+++ layers/+web-services/llm-client/packages.el
@@ -30,6 +30,8 @@
     (gptel-quick :toggle llm-client-enable-gptel
                  :location (recipe :fetcher github
                                    :repo "/karthink/gptel-quick" :files ("*.el")))
+    minuet
+    aider
     (aidermacs :location built-in)
     ;; (aidermacs :location
     ;;        (recipe :fetcher github
@@ -161,4 +163,66 @@ Spacemacs leader key).  For example, `SPC y e r` calls
     :load-path "~/git/aidermacs"
     :config
     (spacemacs/set-leader-keys
-      "yw" 'aidermacs-transient-menu)))
+      "aa" 'aidermacs-transient-menu)))
+
+(defun llm-client/init-aider ()
+  (use-package aider
+    :config
+    ;; (setq aider-args '("--model" "sonnet" "--no-auto-accept-architect"))
+    ;; (setenv "ANTHROPIC_API_KEY" anthropic-api-key)
+    ;; Or gemini model
+    ;; (setq aider-args '("--no-gitignore" "--no-auto-commits" "--no-show-model-warnings" "--model" "gemini/gemini-2.0-flash"))
+    ;; (setq aider-args '("--model" "gemini"))
+    ;; (setenv "GEMINI_API_KEY" <your-gemini-api-key>)
+    ;; Or chatgpt model
+    ;; (setq aider-args '("--model" "o4-mini"))
+    ;; (setenv "OPENAI_API_KEY" <your-openai-api-key>)
+    ;; Or use your personal config file
+    ;; (setq aider-args `("--config" ,(expand-file-name "~/.aider.conf.yml")))
+    ;; ;;
+    ;; Optional: Set a key binding for the transient menu
+    ;; or use aider-transient-menu-2cols / aider-transient-menu-1col, for narrow screen
+    (global-set-key (kbd "C-c SPC") 'aider-transient-menu)))
+
+(defun llm-client/init-minuet ()
+  (use-package minuet
+    :bind
+    (;; ("C-c y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
+     ;; ("C-c i" . #'minuet-show-suggestion) ;; use overlay for completion
+     ;; ("C-c m" . #'minuet-configure-provider)
+     :map minuet-active-mode-map
+     ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
+     ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
+     ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
+     ("M-A" . #'minuet-accept-suggestion) ;; accept whole completion
+     ;; Accept the first line of completion, or N lines with a numeric-prefix:
+     ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
+     ("M-a" . #'minuet-accept-suggestion-line)
+     ("M-e" . #'minuet-dismiss-suggestion))
+
+    :init
+    ;; if you want to enable auto suggestion.
+    ;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
+    ;; (add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
+
+    :config
+    ;; You can use M-x minuet-configure-provider to interactively configure provider and model
+    (setq minuet-provider 'openai)
+
+    ;; For Evil users: When defining `minuet-ative-mode-map` in insert
+    ;; or normal states, the following one-liner is required.
+
+    ;; (add-hook 'minuet-active-mode-hook #'evil-normalize-keymaps)
+
+    ;; This is *not* necessary when defining `minuet-active-mode-map`.
+
+    ;; To minimize frequent overhead, it is recommended to avoid adding
+    ;; `evil-normalize-keymaps` to `minuet-active-mode-hook`. Instead,
+    ;; bind keybindings directly within `minuet-active-mode-map` using
+    ;; standard Emacs key sequences, such as `M-xxx`. This approach should
+    ;; not conflict with Evil's keybindings, as Evil primarily avoids
+    ;; using `M-xxx` bindings.
+
+    (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 64)
+    (minuet-set-optional-options minuet-openai-options :max_tokens 64)))
+
