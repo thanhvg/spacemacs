c994fee8b1fc4d9fcdec7d05e0cd2138551550cd
Thanh Vuong <thanhvg@gmail.com>
2025-08-11 13:54:30 -0600

    [source-control] lexical nil for funcs
    because of shadowing git-link-open-in-browser in various functions
    
    An example
    
    (defun spacemacs/git-link-copy-url-only ()
      "Only copy the generated link to the kill ring."
      (interactive)
      (let (git-link-open-in-browser)
        (call-interactively 'git-link)))
    
    when it git-link is lazy loaded then the variable git-link-open-in-browser is
    not defined yet. So defvar of it will be run after it is defined here. And hence
    error is thrown
    
    most of the lexical errors would be like this

--- layers/+source-control/git/funcs.el
+++ layers/+source-control/git/funcs.el
@@ -1,4 +1,4 @@
-;;; funcs.el --- Colors Layer functions File  -*- lexical-binding: t; -*-
+;;; funcs.el --- Colors Layer functions File  -*- lexical-binding: nil; -*-
 ;;
 ;; Copyright (c) 2012-2025 Sylvain Benner & Contributors
 ;;
